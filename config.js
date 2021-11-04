7fac97bd51f878a6249bcab2f03fc11ee87d9f01
Aadarsh777 <88802572+Aadarsh777@users.noreply.github.com>
2021-11-04 15:07:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports= {
-    MONGODB : 'mongodb+srv://admin:aDAhZ48Sd4sh2OsC@cluster0.gu2ll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    MONGODB : 'mongodb+srv://<dbUserName>:<admin-password>C@cluster0.gu2ll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     SECRET_KEY: 'some very secret key'
 }
 
