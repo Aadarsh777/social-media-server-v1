4c55c63fe04fdfcfd694367d8745f38b1df5d34b
Aadarsh Raj <aadarshraj12345678@gmail.com>
2021-11-04 15:05:47 +0530

    First commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports= {
+    MONGODB : 'mongodb+srv://admin:aDAhZ48Sd4sh2OsC@cluster0.gu2ll.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+}
+
+
